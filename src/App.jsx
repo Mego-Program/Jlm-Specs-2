461259df3cffe9a29ea8816e31e1bebce2647bbd
David Sebban <davsebban13@gmail.com>
2023-11-01 14:45:33 +0200

    first-code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import React, { useState } from 'react';
+import Task from './Task';
+import AddTaskButton from './AddTaskButton';
 
-function App(){
-    <div>
+function App() {
+    const [tasks, setTasks] = useState([]);
 
-    </div>
+    const addNewTask = () => {
+        const newTask = {
+            date: "2023-11-02",
+            title: "New task",
+            content: "New task content",
+            status: "In progress",
+            assignedTo: "New user"
+        };
+
+        setTasks([...tasks, newTask]);
+    };
+
+    return (
+        <div className="App">
+            <AddTaskButton onClick={addNewTask} />
+            {tasks.map((task, index) => (
+                <Task
+                    key={index}
+                    date={task.date}
+                    title={task.title}
+                    content={task.content}
+                    status={task.status}
+                    assignedTo={task.assignedTo}
+                />
+            ))}
+        </div>
+    );
 }
+
 export default App;
+
