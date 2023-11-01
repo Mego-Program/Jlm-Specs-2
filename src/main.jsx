87212317d99b07efa29a520c1b7e3b00d2335c32
abrahamfaust <123985542+abrahamfaust@users.noreply.github.com>
2023-11-01 16:10:25 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import SpecsApp from './SpecsApp'
 import './index.css'
-import SpecApp from "./SpecsApp"
-import './SpecObject.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
