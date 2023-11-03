e046791a982a1ac90f489656dd779248441685ef
Abraham <faustabraham@gmail.com>
2023-11-03 12:21:22 +0200

    added a router and a folder pages for all three pages

---
+++ src/pages/SpecsList.jsx
@@ -0,0 +1,20 @@
+// import React, { useState } from 'react';
+import { Button } from "@mui/material";
+import SpecObject from "../components/SpecObject";
+import BasicStack from "../components/test";
+import { Link } from "react-router-dom";
+
+function SpecsList() {
+  return (
+    <div>
+      <h1>Specs List</h1>
+      <Link to="./SpecInfo">info</Link>
+      <br />
+      <Link to="./SpecKpi">kpi</Link>
+      {/* <BasicStack /> */}
+      {/* <SpecObject/> */}
+    </div>
+  );
+}
+
+export default SpecsList;
