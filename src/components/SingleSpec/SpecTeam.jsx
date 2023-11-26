d07c64b93ab934722801c7609020f93000ddd1ea
David Sebban <davsebban13@gmail.com>
2023-11-26 16:39:10 +0200

    small fixes on single spec page

--- src/components/SingleSpec/SpecTeam.jsx
+++ src/components/SingleSpec/SpecTeam.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Box, List, ListItem, Typography } from "@mui/material";
+
 
 function SpecTeam({ team }) {
   return (
     <div>
-      <h3>Team:</h3>
+      <Typography variant="h5">Team:</Typography>
       <ul>
         {team.map((user, index) => (
           <li key={index}>{user}</li>
